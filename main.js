bdfbba9e0746ed67b39cf3de590f1d98a3379256
khaledSoliman <khaledx.lap@gmail.com>
2020-06-17 20:31:37 +0200

    add animation

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
-  container.classList.add('right-panal-active');
+  container.classList.add('right-panel-active');
 });
 
 signInButton.addEventListener('click', () => {
-  container.classList.remove('right-panal-active');
+  container.classList.remove('right-panel-active');
 });
